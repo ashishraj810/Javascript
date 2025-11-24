ba649427e3dd8897d6dbefe9b25ffecf2de4db28
ashishraj810 <ashishraj08105@gmail.com>
2025-11-25 00:04:07 +0530

    Add files via upload

---
+++ String.js
@@ -0,0 +1,21 @@
+const str = "Ashish Raj Ashish Raj ";
+const str1= `Ashish 
+Raj`;
+
+
+console.log(str);
+console.log(str1);
+console.log(str.toUpperCase());                 // convert to upper case
+console.log(str.toLowerCase());                  // convert to lower case
+console.log(str.indexOf('aj'));                 // to check at which index the string appeared fisrt
+console.log(str.lastIndexOf('aj'));             //to check at which index the string appeared last 
+console.log(str.includes('xc'));                //to check whether the string is present inside the given set of string                      
+console.log(str.replace("sh" , "kk"));          //replaces string appearing first
+console.log(str.replaceAll("sh" , "kk"));          //replaces all the  string appearing in the set of string
+console.log(str.trim());                        //removes the whitespace from starting and end of the string and not from in between the string
+
+
+
+ 
+
+
