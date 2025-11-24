656e355c01d6113804c3995e89955959a60ddbd6
ashishraj810 <ashishraj08105@gmail.com>
2025-11-24 18:18:30 +0530

    Add files via upload

---
+++ Map.js
@@ -0,0 +1,8 @@
+// Map in js is used to make modifications in elements and return the result  
+
+const arr = [3 , 5 , 10 , 12 , 15 , 20];
+
+const arr2 = arr.map((nums) => nums*5);
+
+
+console.log(arr2);
